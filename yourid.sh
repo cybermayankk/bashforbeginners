e9b75b911bca90fcb5602c88a83f09e4343c0eb5
Mayank Gandhi <86929443+cybermayankk@users.noreply.github.com>
2021-10-11 09:59:57 -0700

    Create yourid.sh

---
+++ yourid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# I OWN THIS COMMAND AFTER ALL VIDEOS 
+
+Name=Mayank
+Surname=Gandhi
+
+echo "My name is $Name and my surname is $Surname , My username is $SHELL , My id is $HOSTNAME and my home directory is $HOME & its version is $BASH_VERSION"
