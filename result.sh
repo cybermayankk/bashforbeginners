2df04d046fa3511b9ff2cc594763f214b4097749
Mayank Gandhi <86929443+cybermayankk@users.noreply.github.com>
2021-10-11 10:00:57 -0700

    Create result.sh

---
+++ result.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# In which You  can see we create a result
+
+# If we have two numbers and we want to add those follow me 
+# and type that command 
+
+number1=123
+number2=124
+
+result=$(( number1+number2 ))
+
+echo "Result is here $result"
+
+# we also type result like this 
+
+# result=`expr $number1 + $number2`
